fe005b77f119789d52d93438720bdd880c9d7f5c
Miguel Osorio @kelvur <Osorio10.92@gmail.com>
2020-01-23 23:41:25 +0100

    doc: the unclear behavior of this.remove

--- src/Queue/Queue.js
+++ src/Queue/Queue.js
@@ -12,6 +12,12 @@ export default class Queue extends LinkedList{
   }
 
   remove(){
+    // Don't call to this.remove because
+    // cause a infinity call chain.
+    // this.remove doesn't reference
+    // this own function, the reason is
+    // because how LinkedList add side effects
+    // to the functions
     return super.remove(0);
   }
 
