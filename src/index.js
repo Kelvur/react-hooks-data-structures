fbad05d8665b5ec44084cdda32be9657f919b80f
Miguel Osorio @kelvur <Osorio10.92@gmail.com>
2020-02-11 22:28:38 +0100

    fix: missing export of Stack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import * as LinkedList from './LinkedList';
 import * as Queue from './Queue';
+import * as Stack from './Stack';
 
 
 const DataStructures = {
   LinkedList,
   Queue,
+  Stack,
 };
 
 export default DataStructures;
